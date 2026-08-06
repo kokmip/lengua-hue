39a6482c5e72916639e751080c496e824c0b08bf
kokmip <tsvetocheck3@gmail.com>
2026-08-06 14:19:28 +0200

    feat: add Word database model for SQLite

---
+++ src/database/connection.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+import os 
+from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class Base(DeclarativeBase):
+    pass
+
